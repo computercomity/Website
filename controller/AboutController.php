6fbcd50d3ab430e6b7dffaeb046c2b524f2407a6
Xiaoxing Ye <ye@xiaoxing.us>
2015-12-13 01:26:22 +0800

    view: remore unnecessary demo action

--- controller/AboutController.php
+++ controller/AboutController.php
@@ -57,29 +57,4 @@ class AboutController
         return send_result( $data );
     }
 
-    /**
-     * 系统提示
-     * @ApiDescription(section="Demo", description="系统提示")
-     * @ApiLazyRoute(uri="/info",method="GET")
-     * @ApiReturn(type="object", sample="{'code': 0,'message': 'success'}")
-     */
-    public function info()
-    {
-        //$data['notice'] = ;
-        return send_error('SYSTEM','这里是信息提示页面');
-    }
-    
-    /**
-     * Demo接口
-     * @ApiDescription(section="Demo", description="乘法接口")
-     * @ApiLazyRoute(uri="/demo/times",method="GET")
-     * @ApiParams(name="first", type="string", nullable=false, description="first", check="check_not_empty", cnname="第一个数")
-     * @ApiParams(name="second", type="string", nullable=false, description="second", check="check_not_empty", cnname="第二个数")
-     * @ApiReturn(type="object", sample="{'code': 0,'message': 'success'}")
-     */
-    public function demo($first,$second)
-    {
-        return send_result(intval($first)*intval($second));
-    }
-
 }
