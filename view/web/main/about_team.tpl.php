98cfa4a3eb1715b73ba7a36b0c6fb2e4669a94e0
Xiaoxing Ye <ye@xiaoxing.us>
2016-01-10 00:03:17 +0800

    disable

--- view/web/main/about_team.tpl.php
+++ view/web/main/about_team.tpl.php
@@ -19,22 +19,22 @@
         <div class="portfolio-holder" id="Grid">
             <!--mix后面是部门名称，动画用-->
             <?php foreach($data['team']['members'] as $member) { ?>
-            <div class="portfolio-post col-sm-6 col-md-3 mix <?php=$member['catagory_id']?>">
+            <div class="portfolio-post col-sm-6 col-md-3 mix <?php=$member['catagory_id'];?>">
                 <div class="thumb-post">
                     <div class="overlay">
                         <div class="overlay-inner">
                             <div class="portfolio-infos">
-                                <span class="meta-category"><?php=$member['catagory']?></span>
-                                <h3 class="portfolio-title"><?php=$member['name']?></h3>
+                                <span class="meta-category"><?php=$member['catagory'];?></span>
+                                <h3 class="portfolio-title"><?php=$member['name'];?></h3>
                             </div>
                             <div class="portfolio-expand">
-                                <a class="fancybox" href="images/team/<?php=$member['image']?>" title="<?php=$member['name']?>">
+                                <a class="fancybox" href="images/team/<?php=$member['image'];?>" title="<?php=$member['name'];?>">
                                     <i class="fa fa-expand"></i>
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <img src="images/team/<?=$member['image']?>" alt="<?php=$member['name']?>">
+                    <img src="images/team/<?php=$member['image']?>" alt="<?php=$member['name']?>">
                 </div>
             </div>
             <!-- /.col-md-3 -->
