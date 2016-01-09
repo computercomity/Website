980177cf1b4d44ab1ef24c988ab12609d5df4354
Xiaoxing Ye <ye@xiaoxing.us>
2016-01-10 00:06:06 +0800

    Revert "disabled <? short code on team page"
    This reverts commit ecc60822556ae304a30746a70e74dda0911013b2.
    
    Conflicts:
    	view/web/main/about_team.tpl.php

--- view/web/main/about_team.tpl.php
+++ view/web/main/about_team.tpl.php
@@ -18,27 +18,44 @@
     <div class="row">
         <div class="portfolio-holder" id="Grid">
             <!--mix后面是部门名称，动画用-->
+<<<<<<< HEAD
             <?php foreach($data['team']['members'] as $member) { ?>
             <div class="portfolio-post col-sm-6 col-md-3 mix <?php=$member['catagory_id'];?>">
+=======
+            <? foreach($data['team']['members'] as $member) { ?>
+            <div class="portfolio-post col-sm-6 col-md-3 mix <?=$member['catagory_id']?>">
+>>>>>>> parent of ecc6082... disabled <? short code on team page
                 <div class="thumb-post">
                     <div class="overlay">
                         <div class="overlay-inner">
                             <div class="portfolio-infos">
+<<<<<<< HEAD
                                 <span class="meta-category"><?php=$member['catagory'];?></span>
                                 <h3 class="portfolio-title"><?php=$member['name'];?></h3>
                             </div>
                             <div class="portfolio-expand">
                                 <a class="fancybox" href="images/team/<?php=$member['image'];?>" title="<?php=$member['name'];?>">
+=======
+                                <span class="meta-category"><?=$member['catagory']?></span>
+                                <h3 class="portfolio-title"><?=$member['name']?></h3>
+                            </div>
+                            <div class="portfolio-expand">
+                                <a class="fancybox" href="images/team/<?=$member['image']?>" title="<?=$member['name']?>">
+>>>>>>> parent of ecc6082... disabled <? short code on team page
                                     <i class="fa fa-expand"></i>
                                 </a>
                             </div>
                         </div>
                     </div>
+<<<<<<< HEAD
                     <img src="images/team/<?php=$member['image']?>" alt="<?php=$member['name']?>">
+=======
+                    <img src="images/team/<?=$member['image']?>" alt="<?=$member['name']?>">
+>>>>>>> parent of ecc6082... disabled <? short code on team page
                 </div>
             </div>
             <!-- /.col-md-3 -->
-            <?php}?>
+            <?}?>
         </div> <!-- /.portfolio-holder -->
     </div> <!-- /.row -->
 </div> <!-- /.container -->
